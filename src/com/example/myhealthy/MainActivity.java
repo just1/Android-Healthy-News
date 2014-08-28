9de69127622b3b86437d016166d0e8ec19668a62
JHYin <472706330@qq.com>
2014-08-28 19:17:17 +0800

    新建工程，初始化项目

---
+++ src/com/example/myhealthy/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myhealthy;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+
+}
