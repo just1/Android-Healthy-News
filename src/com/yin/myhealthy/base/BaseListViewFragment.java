692c6350bc1fca3fc1c70842a8413fade6bcb897
JHYin <472706330@qq.com>
2014-10-17 16:13:46 +0800

    1.修改BaseListViewFragment，让每个Viewpager的子页面，在加载数据的时候显示"正在加载"，而不会一片空白 2.BaseListViewFragment,进行数据操作的时候，改在setUserVisibleHint()函数里面加载数据

--- src/com/yin/myhealthy/base/BaseListViewFragment.java
+++ src/com/yin/myhealthy/base/BaseListViewFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.yin.myhealthy.GlobalDate;
 import com.yin.myhealthy.R;
@@ -19,7 +20,7 @@ import com.yin.myhealthy.adapter.PullDownListViewAdapter;
 import com.yin.myhealthy.view.PullDownListView;
 import com.yin.myhealthy.view.PullDownListView.OnRefreshListener;
 
-public abstract class BaseListViewFragment extends Fragment{
+public abstract class BaseListViewFragment extends Fragment {
 
 	protected Context context = null;
 	protected LinearLayout ll;
@@ -27,28 +28,30 @@ public abstract class BaseListViewFragment extends Fragment{
 	protected PullDownListViewAdapter adapter;
 	protected String apiUrl = null;
 	protected BaseListController controller;
-	protected String id;	// 类别的id号
+	protected String id; // 类别的id号
 
-	
+	protected TextView tv_loading;
+	protected boolean isHaveData = false; // 是否已经有数据了
+
 	public BaseListViewFragment(String url, String id) {
 		this.id = id;
 		apiUrl = url;
 	}
-	
-	
+
 	@Override
 	public void onAttach(Activity activity) {
-	    super.onAttach(activity);  
-	    context = activity;  
+		super.onAttach(activity);
+		context = activity;
 	}
-	
-	public void setController(BaseListController ctrl){
+
+	public void setController(BaseListController ctrl) {
 		controller = ctrl;
 	}
-	
+
 	// 初始化ListView
 	private void initView() {
 		lv = new PullDownListView(context);
+
 		ll.addView(lv);
 
 		adapter = new PullDownListViewAdapter(controller.getTitleList(),
@@ -89,23 +92,40 @@ public abstract class BaseListViewFragment extends Fragment{
 		// 设置listview的item监听
 		setLvItemClickListener();
 	}
-	
-	
+
+	private void updataViewVisible() {
+		if (isHaveData) {
+			lv.setVisibility(View.VISIBLE);
+			tv_loading.setVisibility(View.GONE);
+		} else {
+			lv.setVisibility(View.GONE); // 设置不显示先，直到有数据再来显示
+			tv_loading.setVisibility(View.VISIBLE);
+		}
+
+	}
+
+	private void initData() {
+		// 请求网络，获取健康知识分类列表
+		controller.getListData(apiUrl, clickCount, id, handler);
+	}
+
 	// 当数据返回来时回调
 	Handler handler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
 			if (msg.what == GlobalDate.GET_DATA_SUCCESS) {
 
+				isHaveData = true;
+				updataViewVisible();
+
 				// 更新ListView数据
 				adapter.notifyDataSetChanged();
 			}
 		}
 	};
-	
 
 	abstract public void setLvItemClickListener();
-	
+
 	// "加载更多"的事件处理
 	protected int clickCount = 1; // 记录按下的次数
 	OnClickListener moreBtnListener = new OnClickListener() {
@@ -117,21 +137,41 @@ public abstract class BaseListViewFragment extends Fragment{
 			controller.getListData(apiUrl, clickCount, id, handler);
 		}
 	};
-	
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_base_lv, container,
 				false);
 
-		ll = (LinearLayout) view.findViewById(R.id.ll_frag_healthyknow);
+		ll = (LinearLayout) view.findViewById(R.id.ll_frag);
+		tv_loading = (TextView) view.findViewById(R.id.tv_loading);
 
 		// 初始化ListView
 		initView();
-		// 请求网络，获取健康知识分类列表
-		controller.getListData(apiUrl, clickCount, id, handler);
-		
+		updataViewVisible();
+
+		// initData();
+
 		return view;
+	}
+
+	@Override
+	public void setUserVisibleHint(boolean isVisibleToUser) {
+		// TODO Auto-generated method stub
+		if (isVisibleToUser) { // 如果可见
+
+			if (controller.getTitleList().size() > 0) { // 如果已经有数据，则不用访问网络
+				isHaveData = true;
+				updataViewVisible();
+			} else { // 没有数据才访问网络
+				initData(); // 加载数据
+			}
+
+		} else { // 如果不可见，不执行操作
+
+		}
+
+		super.setUserVisibleHint(isVisibleToUser);
 	}
 }
